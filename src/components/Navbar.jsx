c1391328b846097271fdcca5ee822d53ab0c0a7d
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-01-15 20:30:31 +0000

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { menu, close } from '../assets'
+
 import { navLinks } from '../constants'
 import { styles } from '../styles'
 
@@ -8,6 +10,7 @@ import { styles } from '../styles'
 const Navbar = () => {
 
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav
@@ -30,9 +33,9 @@ const Navbar = () => {
             <li
               key={link.id}
               className={`${active === link.title
-                  ? "text-white"
-                  : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
+                ? "text-white"
+                : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}
             >
               <a href={`#${link.id}`}>{link.title}</a>
@@ -41,6 +44,36 @@ const Navbar = () => {
           }
 
         </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${!toggle ? "hidden" : "flex"
+              } p-6 bg-black absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${active === link.title ? "text-white" : "text-secondary"
+                    }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
       </div>
 
 
