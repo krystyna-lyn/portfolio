0588f1cd72b3e06a943e51c5a15ae8ea81a944c8
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-01-30 18:55:58 +0100

    update responsive

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -17,12 +17,12 @@ const Single = ({ item }) => {
   return (
     <section id="works" className="mt-32">
       <div className="flex items-center justify-center size-full overflow-hidden">
-        <div className="max-w-[1100px] h-full m-auto flex items-center justify-center gap-12">
-          <div className="flex-1 h-[50%] sm:w-full sm:max-h-[300px]" ref={ref}>
-            <img className="w-full h-full object-cover sm:object-contain" src={item.img} alt="" />
+        <div className="max-w-[1100px] h-full m-auto flex flex-col items-center justify-center gap-12 sm:flex-row">
+          <div className="sm:flex-1 sm:h-[50%] w-full max-h-[300px]" ref={ref}>
+            <img className="size-full object-contain sm:object-cover" src={item.img} alt="" />
           </div>
-          <motion.div className="flex-1 flex flex-col gap-5" style={{ y }}>
-            <h2>{item.title}</h2>
+          <motion.div className={`flex-1 flex flex-col gap-5 p-3 items-center text-center ${styles.tramsform}`}>
+            <h2 className="text-[30px] sm:text-[40px]">{item.title}</h2>
             <p>{item.desc}</p>
             <button className="bg-[#915EFF] border-none rounded-lg p-2 w-[200px] cursor-pointer">
               <a href={item.url} target="_blank">See Demo</a>
@@ -49,13 +49,12 @@ const Portfolio = () => {
 
   return (
     <div className="relative" ref={ref}>
-      <div className={`${styles.heroSubText} sticky top-0 left-0 pt-5 text-center text-[36px] sm:text-[24px]`}>
+      <div className={`${styles.heroSubText} sticky top-0 left-0 pt-5 text-center`}>
 
         <h1>Featured Works</h1>
 
         <motion.div style={{ scaleX }} className="h-2 bg-white-200"></motion.div>
       </div>
-      {/* Use flex classes to create a vertical layout */}
       <div className="flex flex-col gap-12 items-center">
         {items.map((item) => (
           <Single item={item} key={item.id} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
+  important: true,
   content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
