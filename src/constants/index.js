888052c396ad45611f6cda3f120eaab5010d2bec
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-03-16 20:12:36 +0000

    update portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -26,8 +26,17 @@ export const navLinks = [
 ];
 
 const items = [
+
   {
     id: 1,
+    title: "WaveTube Video App",
+    img: "/portfolio/project-5.png",
+    desc: "created using React.js, MUI",
+    url: "https://wavetubevideos.netlify.app"
+  },
+
+  {
+    id: 2,
     title: "Music App",
     img: "/portfolio/project-1.png",
     desc: "created using HTML, CSS, JavaScript",
@@ -35,29 +44,30 @@ const items = [
 
   },
   {
-    id: 2,
-    title: "WaveTube Video App",
-    img: "/portfolio/project-5.png",
-    desc: "created using React",
-    url: "https://wavetubevideos.netlify.app"
-  },
-  {
     id: 3,
-    title: "E-Commerce Website",
-    img: "/portfolio/project-4.png",
-    desc: "created using Angular",
-    url: "https://github.com/krystyna-lyn/nb-ks-shop"
+    title: "Green Camp UI/UX Website",
+    img: "/portfolio/project-7.png",
+    desc: "created using React.js, Next.js, Tailwind CSS",
+    url: "https://greencamptravel.netlify.app/"
   },
 
   {
     id: 4,
+    title: "E-Commerce Website",
+    img: "/portfolio/project-4.png",
+    desc: "created using Angular, see code below",
+    url: "https://github.com/krystyna-lyn/nb-ks-shop"
+  },
+
+  {
+    id: 5,
     title: "E-Commerce Shop",
     img: "/portfolio/project-2.png",
     desc: "created using HTML, CSS, JavaScript",
     url: "https://krystyna-lyn.github.io/e-shop/"
   },
   {
-    id: 5,
+    id: 6,
     title: "Coffee Shop Website",
     img: "/portfolio/project-6.png",
     desc: "created using PHP",
@@ -65,21 +75,21 @@ const items = [
   },
   
   {
-    id: 8,
+    id: 7,
     title: "Hotel booking Website UI/UX",
     img: "/portfolio/project-10.png",
     desc: "created using HTML, CSS",
     url: "https://krystyna-lyn.github.io/hotel-web/"
   },
   {
-    id: 9,
+    id: 8,
     title: "Fashion Shop Website",
     img: "/portfolio/project-11.png",
     desc: "created using Shopify",
     url: "#"
   },
   {
-    id: 10,
+    id: 9,
     title: "Shoes Shop UI/UX",
     img: "/portfolio/project-9.png",
     desc: "created using React",
